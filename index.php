5fbedb9f7daa0d03fc48a4ab23515c908be76da5
HHH <hayden@opie-etc.com>
2025-05-13 11:48:38 +0800

    Add the index file

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+if (!file_exists(__DIR__ . '/env.php')) {
+    die('Missing env.php. Copy env.php.sample, update it, and save it as env.php to begin using the scripts.');
+}
+require __DIR__ . '/env.php';
+?>
